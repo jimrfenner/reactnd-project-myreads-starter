2aacc5ee58e5c344712c26f79f76d606f1bc1d8a
Jim Fenner <jimrfenner@gmail.com>
2018-01-04 21:53:42 -0500

    switching branches

--- src/App.js
+++ src/App.js
@@ -65,10 +65,6 @@ class BooksApp extends React.Component {
                                   'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
                               }}
                             />
-                            <ShelfSwitcher />
-                            <ShelfSwitcher />
-                            <ShelfSwitcher />
-                            <ShelfSwitcher />
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="none" disabled>
