1cc9e3ca741665162c5b651d82cb4c30c6bc6957
Jim Fenner <jimrfenner@gmail.com>
2018-01-09 22:25:57 -0500

    feat: Retrieve books from API with componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import Search from './Search';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
+    books: []
   };
 
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
+
   render() {
+    const { books } = this.state;
     return (
       <div className="app">
         <Route exact path="/search" render={() => <Search />} />
-        <Route exact path="/" render={() => <ListBooks />} />
+        <Route exact path="/" render={() => <ListBooks books={books} />} />
       </div>
     );
   }
