86b6f5e6d55bd46b8315c18a742d10811c7ce6fb
Jim Fenner <jimrfenner@gmail.com>
2018-01-14 21:00:43 -0500

    refactor: Change Book to functional component and fix cover link

--- src/Book.js
+++ src/Book.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Book extends Component {
-  static PropTypes = {
-    title: PropTypes.string.isRequired,
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    authors: PropTypes.string.isRequired,
-    backgroundImage: PropTypes.string.isRequired
-  };
-  render() {
-    const { book } = this.props;
-    const style = {
-      width: book.width,
-      height: book.height,
-      backgroundImage: book.url
-    };
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={style} />
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+const Book = props => {
+  const { book } = props;
+
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 192,
+            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
       </div>
-    );
-  }
-}
+      <div className="book-title">{book.title}</div>
+      {book.authors &&
+        book.authors.length &&
+        book.authors.map((author, index) => (
+          <div key={index} className="book-authors">
+            {author}
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default Book;
